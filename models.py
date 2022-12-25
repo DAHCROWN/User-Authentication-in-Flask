a9c1adcaaa433db6f94ed09883f44968362092fc
DAHCROWN <57120458+DAHCROWN@users.noreply.github.com>
2022-12-26 00:09:01 +0100

    m,

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from flask_login import UserMixin
 import sqlalchemy as mod
 
 class User(UserMixin, db.Model):
-    # __tablename__ = "user"
+    __tablename__ = "user"
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
