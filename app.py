d2031b5a3810612beb95f29ae80280da9e71c66a
DAHCROWN <57120458+DAHCROWN@users.noreply.github.com>
2022-12-25 17:33:40 +0100

    asdf

--- app.py
+++ app.py
@@ -28,10 +28,9 @@ def create_app():
     app = Flask(__name__)
 
     app.secret_key = 'secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://hocjseczzxrzkz:11f9f8ae1a833f1bc24c96fadf05b42424665dd8157c2ad9d58d8ccbbb3652c7@ec2-34-231-63-30.compute-1.amazonaws.com:5432/d4hvpctm1e1un4"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    pymysql.install_as_MySQLdb()
     login_manager.init_app(app)
     db.init_app(app)
     migrate.init_app(app, db)
